3552d435b10de14d6c878d55572354cdc8097919
mperdikeas <mperdikeas@gmail.com>
2019-09-13 13:49:29 -0400

    hue control added

--- app/app.js
+++ app/app.js
@@ -18,14 +18,18 @@ class App extends React.Component {
         super(props);
         this.updateStart = this.updateStart.bind(this);
         this.updateRotations = this.updateRotations.bind(this);
-        this.state = {start: 0.5, rotations: -1.5};
+        this.updateHue = this.updateHue.bind(this);
+        this.state = {start: 0.5, rotations: -1.5, hue: 1.2};
     }
 
-    updateStart(start) {
-        this.setState({start: start});
+    updateStart(x) {
+        this.setState({start: x});
     }
-    updateRotations(rotations) {
-        this.setState({rotations: rotations});
+    updateRotations(x) {
+        this.setState({rotations: x});
+    }
+    updateHue(x) {
+        this.setState({hue: x});
     }
     render() {
         return (
@@ -33,18 +37,22 @@ class App extends React.Component {
                 <h1>Demo of the cubehelix implementation</h1>
                 <Controls updateStart={this.updateStart}
                           updateRotations={this.updateRotations}
+                          updateHue={this.updateHue}
                           start={this.state.start}
                           rotations={this.state.rotations}
+                          hue={this.state.hue}
                 />
                 <ColourStripe width={this.props.width}
                               height={50}
                               start={this.state.start}
                               rotations={this.state.rotations}
+                              hue={this.state.hue}
                 />
                 <ColourMap width={this.props.width}
                            height={600}
                            start={this.state.start}
                            rotations={this.state.rotations}
+                           hue={this.state.hue}
                 />
                 </div>
         );
@@ -62,6 +70,7 @@ class Controls extends React.Component {
                 <div>
                 <StartControl value={this.props.start} updateStart={this.props.updateStart}/>
                 <RotationsControl value={this.props.rotations} updateRotations={this.props.updateRotations}/>
+                <HueControl value={this.props.hue} updateHue={this.props.updateHue}/>
                 </div>
         );
     }
@@ -70,8 +79,10 @@ class Controls extends React.Component {
 Controls.propTypes = {
     updateStart: PropTypes.func.isRequired,
     updateRotations: PropTypes.func.isRequired,
+    updateHue: PropTypes.func.isRequired,
     start: PropTypes.number.isRequired,
-    rotations: PropTypes.number.isRequired
+    rotations: PropTypes.number.isRequired,
+    hue: PropTypes.number.isRequired
 };
 
 class StartControl extends React.Component {
@@ -96,13 +107,12 @@ class StartControl extends React.Component {
         );
     }
 }
-
 StartControl.propTypes = {
     updateStart: PropTypes.func.isRequired,
     value: PropTypes.number.isRequired
 };
 
-// TODO: see whether I can create a single class for all ParameterControls
+
 class RotationsControl extends React.Component {
     constructor(props) {
         super(props);
@@ -125,11 +135,39 @@ class RotationsControl extends React.Component {
         );
     }
 }
-
 RotationsControl.propTypes = {
     updateRotations: PropTypes.func.isRequired,
     value: PropTypes.number.isRequired
 };
+
+
+class HueControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: this.props.value};
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    handleInputChange(e) {
+        this.setState({value: e.target.value});
+        this.props.updateHue(parseFloat(e.target.value));
+    }
+    render() {
+        return (
+            <div>
+                <label htmlFor='start'>Hue</label>
+                <input id='start' type='range'
+                   min={0} max={10} step={0.1} value={this.state.value}
+                            onChange={this.handleInputChange}/>
+                <span>{this.state.value}</span> 
+           </div>
+        );
+    }
+}
+HueControl.propTypes = {
+    updateHue: PropTypes.func.isRequired,
+    value: PropTypes.number.isRequired
+};
+
 
 class ColourStripe extends React.Component {
     render() {
@@ -157,7 +195,9 @@ class ColourStripe extends React.Component {
 
     paint() {
         const defaults = {start: 0.5, r:-1.5, hue:1.2, gamma:1.0};
-        const options = Object.assign({}, defaults, {start: this.props.start, r: this.props.rotations});
+        const options = Object.assign({}, defaults, {start: this.props.start
+                                                     , r: this.props.rotations
+                                                     , hue: this.props.hue});
         const l = cubehelix(options);
         const canvas = this.refs.canvas;
         const ctx = canvas.getContext('2d');
@@ -174,8 +214,8 @@ class ColourStripe extends React.Component {
 ColourStripe.propTypes = {
     width : PropTypes.number.isRequired,
     start: PropTypes.number.isRequired,
-    rotations: PropTypes.number.isRequired
-    
+    rotations: PropTypes.number.isRequired,
+    hue: PropTypes.number.isRequired
 };
 
 class ColourMap extends React.Component {
@@ -208,7 +248,10 @@ class ColourMap extends React.Component {
         ctx.fillStyle='white';
         ctx.fillRect(0, 0, this.props.width, this.props.height);
         const defaults = {start: 0.5, r:-1.5, hue:1.2, gamma:1.0};
-        const options = Object.assign({}, defaults, {start: this.props.start, r: this.props.rotations});        const l = cubehelix(options);
+        const options = Object.assign({}, defaults, {start: this.props.start
+                                                     , r: this.props.rotations
+                                                     , hue: this.props.hue});
+        const l = cubehelix(options);        
         ctx.fillStyle='red';
         for (let i = 0; i < this.props.width; i++) {
             const rgb = l(i/(this.props.width-1));
